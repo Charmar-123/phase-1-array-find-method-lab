a7b069f59ed7a0ca3314fc10fcdfcfc843b84b91
Charbel Maroun <charbel-maroun@hotmail.com>
2022-11-13 19:14:43 -0500

    commit tree 2a114b036db85e69677eb6aeabd62ab954bf9471

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array){
+    const winnerYear = array.find(item => item.result ==="W")
+    if (winnerYear !== undefined){
+        return winnerYear.year
+    } else {
+        return undefined
+    }
+
+}
